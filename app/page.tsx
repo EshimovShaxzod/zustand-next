527e2c904fa963ff7e302a6df440af655adaa9b0
EshimovShaxzod <eshimov474@gmail.com>
2025-02-06 23:13:45 +0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import TodoForm from "@/components/todo-forn";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <TodoForm />
+    </div>
+  );
+};
+
+export default Home;
